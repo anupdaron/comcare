678afb9da8cc1e5964d932a7a07c18e6a007b2ea
Anup Bhandari <anup.daron@gmail.com>
2021-01-28 11:24:59 +0545

    database

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -1,4 +1,4 @@
 const MONGO_URL =
-  "mongodb+srv://commcare:Comcare@123456@cluster0.fbj3o.mongodb.net/commcare?retryWrites=true&w=majority";
+  "mongodb+srv://commcare:7Sk1Qyw1iLg7ap4O@cluster0.fbj3o.mongodb.net/commcare?retryWrites=true&w=majority";
 
 module.exports = { MONGO_URL };
