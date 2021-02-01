788bb383fae83ea5e1c635fab830dfd26921ddfa
Anup Bhandari <anup.daron@gmail.com>
2021-02-01 15:18:38 +0545

    fixed profile image

--- routes/getData.js
+++ routes/getData.js
@@ -57,7 +57,9 @@ const saveVisit = (req, res, user_id, paths) => {
   const data = req.body.json;
   let sendAll = false;
   if (Array.isArray(data)) {
-    const oldVisit = data[0].modelVisitList[0].visit_id.split("_");
+    const oldVisit = data[0].modelPatientList[0].modelVisitList[0].visit_id.split(
+      "_"
+    );
     oldVisit = oldVisit[oldVisit.length - 1] - 1;
     oldVisit = oldVisit.join("_");
     VisitList.find({ visit_id: oldVisit }).then((result) => {
@@ -85,7 +87,9 @@ const saveVisit = (req, res, user_id, paths) => {
     });
   } else {
     console.log(data);
-    const oldVisit = data.modelVisitList[0].visit_id.split("_");
+    const oldVisit = data.modelPatientList[0].modelVisitList[0].visit_id.split(
+      "_"
+    );
     oldVisit = oldVisit[oldVisit.length - 1] - 1;
     oldVisit = oldVisit.join("_");
 
